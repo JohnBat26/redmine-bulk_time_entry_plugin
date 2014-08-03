51a9703f5ea2d8381693e4f7d6e904e12bcc1fb9
Eugene Batogov <johnbat26@gmail.com>
2014-08-04 02:10:04 +0400

    fix query without project id

--- app/controllers/bulk_time_entries_controller.rb
+++ app/controllers/bulk_time_entries_controller.rb
@@ -10,7 +10,7 @@ class BulkTimeEntriesController < ApplicationController
   include BulkTimeEntriesHelper
 
   protect_from_forgery :only => [:index, :save]
-  
+
   def index
     @time_entries = [TimeEntry.new(:spent_on => today_with_time_zone.to_s)]
 
@@ -22,8 +22,10 @@ class BulkTimeEntriesController < ApplicationController
     conditions_sql = []
     conditions_params = []
 
-    conditions_sql << "project_id = ?"
-    conditions_params << project_id
+    if project_id && !project_id.empty?
+      conditions_sql << "project_id = ?"
+      conditions_params << project_id
+    end
 
     if assigned_to_id
       conditions_sql << "assigned_to_id = ?"
@@ -35,18 +37,18 @@ class BulkTimeEntriesController < ApplicationController
       conditions_params << false
     end
 
-    @issues = Issue.find(:all, :conditions => [ conditions_sql.join(' AND ') ] + conditions_params, :include => :status)
+    @issues = Issue.find(:all, :conditions => [conditions_sql.join(' AND ')] + conditions_params, :include => :status)
   end
 
   def load_assigned_issues
-    get_issues params[:project_id], params[:assigned_to_id], params[:only_open]
+    get_issues params[:project_id], params[:assigned_to_id], params[:only_open] if params[:project_id]
 
-    render :partial => 'issues_selector', :locals => { :issues => @issues, :rnd => params[:entry_id].split('_')[1]  }
+    render :partial => 'issues_selector', :locals => {:issues => @issues, :rnd => params[:entry_id].split('_')[1]}
   end
-  
-  
+
+
   def save
-    if request.post? 
+    if request.post?
       @time_entries = params[:time_entries]
 
       @time_entries.each_pair do |html_id, entry|
@@ -63,7 +65,7 @@ class BulkTimeEntriesController < ApplicationController
             <div class='flash notice'>
               #{Time.now.strftime('%H:%M')}:
               #{l(:text_time_added_to_project, :hours => @time_entry.hours, :project => @time_entry.project.name)}
-              #{" (#{@time_entry.comments})" unless @time_entry.comments.blank?}.
+          #{" (#{@time_entry.comments})" unless @time_entry.comments.blank?}.
               <a href=\"javascript:void(null)\" onclick=\"$(this).closest('form').remove()\"><img src=\"/images/close.png\" /></a>
             </div>
           "
@@ -71,7 +73,7 @@ class BulkTimeEntriesController < ApplicationController
       end
     end
   end
-    
+
   def add_entry
     begin
       spent_on = Date.parse(params[:date])
@@ -92,17 +94,17 @@ class BulkTimeEntriesController < ApplicationController
 
       @selected_project = @selected_issue.project
 
-      @issues = [ @selected_issue ]
+      @issues = [@selected_issue]
     end
 
-    render :partial => "time_entry", :locals => { :time_entry => @time_entry }
+    render :partial => "time_entry", :locals => {:time_entry => @time_entry}
   end
 
   def time_entries_today
     @entries = TimeEntry.all(
-      :conditions => ["#{TimeEntry.table_name}.user_id = ? AND #{TimeEntry.table_name}.spent_on = ?", find_current_user.id, Date.today],
-      :include => [:activity, :project, {:issue => [:tracker, :status]}],
-      :order => "#{TimeEntry.table_name}.created_on DESC"
+        :conditions => ["#{TimeEntry.table_name}.user_id = ? AND #{TimeEntry.table_name}.spent_on = ?", find_current_user.id, Date.today],
+        :include => [:activity, :project, {:issue => [:tracker, :status]}],
+        :order => "#{TimeEntry.table_name}.created_on DESC"
     )
 
     @user = find_current_user
@@ -116,7 +118,7 @@ class BulkTimeEntriesController < ApplicationController
   def load_activities
     @activities = TimeEntryActivity.all
   end
-  
+
   def load_allowed_projects
     @projects = User.current.projects.find(:all, Project.allowed_to_condition(User.current, :log_time))
   end
